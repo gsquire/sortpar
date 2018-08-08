fcb02b3de2207e6802ef55ad0582b796f91ce790
Garrett Squire <garrettsquire@gmail.com>
2018-08-07 20:10:54 -0700

    add some more integration tests

--- src/integration.rs
+++ src/integration.rs
@@ -1,11 +1,14 @@
+use std::fs::File;
 use std::process::Command;
 
 use assert_cmd::prelude::*;
+use slurp::read_all_to_string;
+use tempfile::tempdir;
 
 #[test]
 fn test_specify_standard_in() {
     let input = "baseball\napple\ncar";
-    let expected = "apple\nbaseball\ncar\n".as_bytes();
+    let expected = b"apple\nbaseball\ncar\n";
 
     let out = Command::main_binary()
         .unwrap()
@@ -16,4 +19,54 @@ fn test_specify_standard_in() {
         .unwrap();
 
     assert_eq!(out.stdout, expected);
+}
+
+#[test]
+fn test_leading_blanks() {
+    let expected = b"a\n    b\n    c\nd\n";
+
+    let out = Command::main_binary()
+        .unwrap()
+        .arg("-b")
+        .arg("test_files/leading_blanks.txt")
+        .unwrap();
+
+    assert_eq!(out.stdout, expected);
+}
+
+#[test]
+fn test_unique() {
+    let expected = String::from("at the beginning\ncool\ntest\nzebra\n");
+
+    let out = Command::main_binary()
+        .unwrap()
+        .arg("-u")
+        .arg("test_files/unique.txt")
+        .unwrap();
+
+    let actual = String::from_utf8(out.stdout).unwrap();
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn test_fold_and_output() {
+    let dir = tempdir().unwrap();
+    let path = dir.path().join("output.txt");
+    let file = File::create(path.clone()).unwrap();
+    let expected = String::from("APPLE\napple\nbike\nBIKE\ncar\nCAR\n");
+
+    let result = Command::main_binary()
+        .unwrap()
+        .arg("-f")
+        .arg("-o")
+        .arg(path.clone())
+        .arg("test_files/fold.txt")
+        .ok();
+
+    assert!(result.is_ok());
+
+    let actual = read_all_to_string(path).unwrap();
+    assert_eq!(actual, expected);
+
+    drop(file);
 }
